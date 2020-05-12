9e5cfc3aedea53e9a57013ee3fd5f8da7fffccf1
LinguList <mattis.list@posteo.de>
2020-05-12 19:11:01 +0200

    update data

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# pharaocoracholaztecan
-CLDF dataset derived from Pharao Hansen's "Investigation of the Relation between Proto-Náhuatl and Proto-Corachol" from 2020
+# CLDF dataset derived from Pharao Hansen's "Investigation of the Relation between Proto-Náhuatl and Proto-Corachol" from 2020
+
+Cite the source dataset as
+
+> Pharao Hansen, Magnus (2020): ¿Familia o vecinos? Investigando la relación entre el proto-náhuatl y el proto-corachol [Family or neighbors? Investigating the relation between Proto-Náhuatl and Proto-Corachol]. 
+
+This dataset is licensed under a CCBY-NC license
+
+Available online at https://magnuspharao.files.wordpress.com/2019/06/familiaovecinoscoracholnahuadata.pdf
+
+## Statistics
+
+
+![Glottolog: 0%](https://img.shields.io/badge/Glottolog-0%25-red.svg "Glottolog: 0%")
+![Concepticon: 95%](https://img.shields.io/badge/Concepticon-95%25-green.svg "Concepticon: 95%")
+![Source: 100%](https://img.shields.io/badge/Source-100%25-brightgreen.svg "Source: 100%")
+
+- **Varieties:** 8
+- **Concepts:** 100
+- **Lexemes:** 721
+- **Sources:** 1
+- **Synonymy:** 1.00
+- **Cognacy:** 721 cognates in 419 cognate sets (272 singletons)
+- **Cognate Diversity:** 0.51
+
+# Contributors
+
+Name | GitHub user | Description | Role
+--- | --- | ---
+Johann-Mattis List | @LinguList | maintainer | Author
+Bryan Allen | | data collector | DataCollector
+
+
---
+++ lexibank_pharaocoracholaztecan.py
@@ -0,0 +1,98 @@
+import attr
+from pathlib import Path
+
+from pylexibank import Concept, Language,FormSpec
+from pylexibank.dataset import Dataset as BaseDataset
+from pylexibank.util import progressbar
+
+import lingpy
+from clldutils.misc import slug
+
+
+@attr.s
+class CustomConcept(Concept):
+    Spanish = attr.ib(default=None)
+    Number = attr.ib(default=None)
+#
+#
+#@attr.s
+#class CustomLanguage(Language):
+#    Latitude = attr.ib(default=None)
+#    Longitude = attr.ib(default=None)
+#    ChineseName = attr.ib(default=None)
+#    SubGroup = attr.ib(default="Bai")
+#    Family = attr.ib(default="Sino-Tibetan")
+#    DialectGroup = attr.ib(default=None)
+
+
+class Dataset(BaseDataset):
+    dir = Path(__file__).parent
+    id = "pharaocoracholaztecan"
+    concept_class = CustomConcept
+    #language_class = CustomLanguage
+    form_spec = FormSpec(
+            separators="/",
+            first_form_only=True)
+
+    def cmd_makecldf(self, args):
+
+        # parse the data from the word document
+        table = [['']] # we except 9 columns
+        with open(self.raw_dir.joinpath('data.txt').as_posix()) as f:
+            previous = []
+            for i, line in enumerate(f):
+                rows = [c.strip() for c in line.split('\t')]
+                if rows[0].replace('.', '').isdigit():
+                    table += [rows]
+                else:
+                    table[-1][-1] += '/'+rows[0]
+                    table[-1] += rows[1:]
+        # load cognates
+        cognates = self.raw_dir.read_csv('cognates.tsv', delimiter='\t')[1:]
+        concepts = {}
+        for concept in self.concepts:
+            idx = '{0}-{1}'.format(
+                        concept['NUMBER'],
+                        slug(concept['SPANISH']))
+            args.writer.add_concept(
+                    ID=idx,
+                    Name=concept['SPANISH'],
+                    Concepticon_ID=concept['CONCEPTICON_ID'],
+                    Concepticon_Gloss=concept['CONCEPTICON_GLOSS'])
+            concepts[concept['SPANISH']] = idx
+            concepts[concept['SPANISH'].lower()] = idx
+        concepts['Frio/(hace frio)'] = concepts['Frio (hace frio)']
+        args.log.info('added concepts')
+
+        args.writer.add_sources()
+        cognacy, counter = {}, 1
+        languages = args.writer.add_languages(lookup_factory='Name')
+        for i, line in progressbar(enumerate(table[1:])):
+            for j, (language, cell) in enumerate(zip(table[0][2:], line[2:])):
+                if cell.strip():
+                    cognate = cognates[i][j]
+                    if cognate == '?':
+                        cid = counter
+                        counter += 1
+                    else:
+                        cid = '{0}-{1}'.format(i, cognate)
+                        if cid in cognacy:
+                            cid = cognacy[cid]
+                        else:
+                            cognacy[cid] = counter
+                            cid = cognacy[cid]
+                            counter += 1
+                    for lexeme in args.writer.add_forms_from_value(
+                            Value=cell.strip(),
+                            Language_ID=languages[language],
+                            Parameter_ID=concepts[line[1]],
+                            Source=['Pharao2020']
+                            ):
+                        args.writer.add_cognate(
+                                lexeme,
+                                Cognateset_ID=cid,
+                                Source=['Pharao2020'])
+
+
+
+
